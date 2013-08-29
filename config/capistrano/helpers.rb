64ee00f5a04563c3f8620f329c113f36989ced7b
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-29 16:38:59 +0400

    ~~~

--- config/capistrano/helpers.rb
+++ config/capistrano/helpers.rb
@@ -6,7 +6,7 @@ def template(from, to)
   # File.absolute_path __FILE__
   abs_path    = File.expand_path File.dirname __FILE__
   script_root = File.dirname abs_path
-  erb         = File.read script_root + "/templates/#{from}"
+  erb         = File.read script_root + "/capistrano/templates/#{from}"
   put ERB.new(erb).result(binding), to
 end
 
