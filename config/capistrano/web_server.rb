c07a72f70f5694096d8f906a3cb8a8a732f6dad8
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-29 16:51:29 +0400

    ~~~

--- config/capistrano/web_server.rb
+++ config/capistrano/web_server.rb
@@ -2,7 +2,7 @@ namespace :web_server do
   %w[start restart stop force force-stop].each do |action|
     desc "cap web_server:#{action}"
     task action do
-      run _join [gemset, "#{current_path}/bin/unicorn #{action}"]
+      run _join [use_gemset, "#{current_path}/bin/unicorn #{action}"]
     end
   end
 end
