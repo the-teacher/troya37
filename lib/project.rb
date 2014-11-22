294e7ba4d66a0f88dac61fb4c99b2d47bc1461c7
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-11-22 23:17:11 +0300

    ~~~

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,12 @@
 module Project
   # Project::AVATARA_URL
   # localhost:3000
-  ADDRESS = "http://troya37.ru"
-  COOKIES_SCOPE = "troya37.ru" # 'localhost:3000' авторизация действительна для всех поддоменов (.poweruser.ru)
-  
+  ADDRESS = "http://ivperchatka.ru"
+  COOKIES_SCOPE = "ivperchatka.ru" # 'localhost:3000' авторизация действительна для всех поддоменов (.poweruser.ru)
+
   AVATARA_DEFAULT = "/uploads/:attachment/default/:style/missing.jpg"
   AVATARA_URL = "/uploads/:attachment/:login/:style/:filename"
-  
+
   BASE_HEADER_DEFAULT = "/uploads/:attachment/default/iv-schools.jpg"
   BASE_HEADER_URL = "/uploads/:attachment/:login/:filename"
 
